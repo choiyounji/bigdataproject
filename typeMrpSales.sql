5be720cab95b6e30116115dbc403f377a48a0606
jooyeon Kim <kylakim@ewhain.net>
2022-11-12 19:21:45 +0900

    index

--- typeMrpSales.sql
+++ typeMrpSales.sql
@@ -1,3 +1,7 @@
 SELECT iType, AVG(iMrp) AS averageMrp
 FROM typeMrpSales
+USE INDEX (idxMrpIncrease)
 GROUP BY iType;
+
+CREATE INDEX idxMrpIncrease ON typeMrpSales(iType);
+ALTER TABLE typeMrpSales DROP INDEX idxMrpIncrease;
