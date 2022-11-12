2d4acf1b8b88e6781ce1f909f6e3e867fde973bc
jooyeon Kim <kylakim@ewhain.net>
2022-11-12 19:20:43 +0900

    ranking

--- typeSales.sql
+++ typeSales.sql
@@ -1,3 +1,6 @@
-SELECT iType, COUNT(iType), SUM(iOutletSales) AS totalSales, AVG(iOutletSales) AS averageSales
+SELECT iType, COUNT(iType), SUM(iOutletSales) AS totalSales, AVG(iOutletSales) AS averageSales, RANK() OVER w AS 'rank',
+         DENSE_RANK() OVER w AS 'denseRank'
 FROM typeSales
-GROUP BY iType;
+GROUP BY iType
+WINDOW w AS (ORDER BY iType);
+
