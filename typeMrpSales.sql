ccae80d3bf9b7b40bfc441ce4cf0e7197330c923
jooyeon Kim <kylakim@ewhain.net>
2022-11-12 19:27:32 +0900

    index

--- typeMrpSales.sql
+++ typeMrpSales.sql
@@ -3,5 +3,8 @@ FROM typeMrpSales
 USE INDEX (idxMrpIncrease)
 GROUP BY iType;
 
+--create index first
 CREATE INDEX idxMrpIncrease ON typeMrpSales(iType);
+
+--drop index
 ALTER TABLE typeMrpSales DROP INDEX idxMrpIncrease;
