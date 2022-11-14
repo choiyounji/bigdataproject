184d21ef45fdb1680968f320e8dd2446ff408df6
spig0126 <spig0126@gmail.com>
2022-11-15 01:27:08 +0900

    ✨ Feat: add dynamic queries

--- fat.php
+++ fat.php
@@ -6,57 +6,71 @@
     </head>
     <body>
         <div class="title">Sales information based on fat </div>
-      
-<table>
-    <th>iFatContent</th>
-    <th>count(iFatContent)</th>
-    <th>totalSales</th>
-    <th>averageSales</th>
+        <table>
+            <th>iFatContent</th>
+            <th>count(iFatContent)</th>
+            <th>totalSales</th>
+            <th>averageSales</th>
+            <?php
+                header('Content-Type: text/html; charset=UTF-8');
+                $mysqli=mysqli_connect("localhost","team21","team21","team21");
+                if(mysqli_connect_errno()){
+                    printf("Connect failed: %s\n", mysqli_error($mysqli));
+                    exit();
+                }
+                else{
+                    $showValues = "";
+                    if($_POST["showValues"] == "High Fat"){
+                        $showValues = "('High Fat')";
+                    }
+                    elseif($_POST["showValues"] == "Low Fat"){
+                        $showValues = "('Low Fat')";
+                    }
+                    elseif($_POST["showValues"] == "Regular"){
+                        $showValues = "('Regular')";
+                    }
+                    else{
+                        $showValues = "('Low Fat', 'Regular', 'High Fat')";
+                    }
 
-<?php
-$value = $_POST['showValues'];
+                    $sql = "SELECT iFatContent, COUNT(iFatContent) AS cnt, SUM(iOutletSales) AS totalSales, AVG(iOutletSales) AS averageSales
+                    FROM fatSales
+                    WHERE iFatContent in ".$showValues."
+                    GROUP BY iFatContent;";
+                    $res = mysqli_query($mysqli, $sql);
+                    if($res){
 
+                        if(mysqli_num_rows($res) == 0){
+                            echo "There are no sales under the chosen conditions.";
+                        }
+                        else{
+                            while($newArray=mysqli_fetch_array($res, MYSQLI_ASSOC)){
+                                $fatContent=$newArray['iFatContent'];
+                                $count=$newArray['cnt'];
+                                $sum=$newArray['totalSales'];
+                                $average=$newArray['averageSales'];
+                                echo "<tr><td>".$fatContent."</td> <td>".$count."</td> <td>".$sum."</td><td>".$average."</td></tr>";
+                            }
 
-header('Content-Type: text/html; charset=UTF-8');
-$mysqli=mysqli_connect("localhost","team21","team21","team21");
-if(mysqli_connect_errno()){
-   printf("Connect failed: %s\n", mysqli_error($mysqli));
-   exit();
-}
-else{
-   
-   $sql = "SELECT iFatContent, COUNT(iFatContent) AS cnt, SUM(iOutletSales) AS totalSales, AVG(iOutletSales) AS averageSales
-   FROM fatSales
-   GROUP BY iFatContent;";
-   $res = mysqli_query($mysqli, $sql);
-   if($res){
-    while($newArray=mysqli_fetch_array($res, MYSQLI_ASSOC)){
-        $fatContent=$newArray['iFatContent'];
-        $count=$newArray['cnt'];
-        $sum=$newArray['totalSales'];
-        $average=$newArray['averageSales'];
-        echo "<tr><td>".$fatContent."</td> <td>".$count."</td> <td>".$sum."</td><td>".$average."</td></tr>";
-    }
-   }
-    else{
-        printf("Could not retrieve records : %s\n", mysqli_error($mysqli));
-    }
-    mysqli_free_result($res);
-    mysqli_close($mysqli);
-}
-?>
-</table>
-
-<div class="radios">
-    <form action="fat.php" method="POST">
-        <p><div class="show"> SHOW: </div></br>
-        
-        <label><input type= "radio" name = "showValues" value="All" <? if($value ==null||$value ==="All"){echo "checked";}?>> All</label></br>
-        <label><input type= "radio" name = "showValues" value="High Fat" <? if($value==="High Fat"){echo "checked";}?>>High Fat<label></br>
-        <label><input type= "radio" name = "showValues" value="Regular" <? if($value==="Regular"){echo "checked";}?>>Regular<label></br>
-        <label><input type= "radio" name = "showValues" value="Low Fat" <? if($value==="Low Fat"){echo "checked";}?>>Low Fat<label><p>
-        <input type="submit" name="submit" value="Run Analysis">
-</div>
-</body>
-
+                        }
+                        
+                    }
+                    else{
+                        printf("Could not retrieve records : %s\n", mysqli_error($mysqli));
+                    }
+                    mysqli_free_result($res);
+                    mysqli_close($mysqli);
+                }
+            ?>
+        </table>
+        <div class="radios">
+            <form action="fat.php" method="POST">
+                <p><div class="show"> SHOW: </div></br>
+                <label><input type= "radio" name = "showValues" value="All" <? if($value ==null||$value ==="All"){echo "checked";}?>> All</label></br>
+                <label><input type= "radio" name = "showValues" value="High Fat" <? if($value==="High Fat"){echo "checked";}?>>High Fat<label></br>
+                <label><input type= "radio" name = "showValues" value="Regular" <? if($value==="Regular"){echo "checked";}?>>Regular<label></br>
+                <label><input type= "radio" name = "showValues" value="Low Fat" <? if($value==="Low Fat"){echo "checked";}?>>Low Fat<label><p>
+                <input type="submit" name="submit" value="Run Analysis">
+        </div>
+    </body>
 </html>
--- type.php
+++ type.php
@@ -4,43 +4,78 @@
         <meta charset="utf-8">
         <link rel="stylesheet" href="result.css">
     </head>
-
-
     <body>
-        <div class="title">Sales information based on Item Type </div>
-      
-<table>
-    <th>iType</th>
-    <th>count</th>
-    <th>totalSales</th>
-    <th>averageSales</th>
-
-
-</table>
-
-<div class="radios">
-    <form action="type.php" method="POST">
-        <p><div class="show"> SHOW item types: </div></br>   
-        <label><input type= "checkbox" name = "showValues" value="All" > All</label></br>
-        <label><input type= "checkbox" name = "showValues" value="Baking Goods">Baking Goods</label></br>
-        <label><input type= "checkbox" name = "showValues" value="Breads">Breads</label></br>
-        <label><input type= "checkbox" name = "showValues" value="Breakfast">Breakfast</label></br>
-        <label><input type= "checkbox" name = "showValues" value="Canned"></label>Canned</br>
-        <label><input type= "checkbox" name = "showValues" value="Dairy"></label>Dairy</br>
-        <label><input type= "checkbox" name = "showValues" value="Frozen Foods"></label>Frozen Foods</br>
-        <label><input type= "checkbox" name = "showValues" value="Fruits and Vegetables"></label>Fruits and Vegetables</br>
-        <label><input type= "checkbox" name = "showValues" value="Hard Drinks"></label>Hard Drinks</br>
-        <label><input type= "checkbox" name = "showValues" value="Health and Hygiene"></label>Health and Hygiene</br>
-        <label><input type= "checkbox" name = "showValues" value="Household"></label>Household</br>
-        <label><input type= "checkbox" name = "showValues" value="Meat"></label>Meat</br>
-        <label><input type= "checkbox" name = "showValues" value="Seafood"></label>Seafood</br>
-        <label><input type= "checkbox" name = "showValues" value="Snack Foods"></label>Snack Foods</br>
-        <label><input type= "checkbox" name = "showValues" value="Soft Drinks"></label>Soft Drinks</br>
-        <label><input type= "checkbox" name = "showValues" value="Starcky Foods"></label>Starcky Foods</br>
-        <label><input type= "checkbox" name = "showValues" value="Others"></label>Others<p>
-        <input type="submit" name="submit" value="Run Analysis">
-</div>
-
-</body>
-
+        <div class="title">Sales information based on Item Type </div>   
+        <table>
+            <th>iType</th>
+            <th>count</th>
+            <th>totalSales</th>
+            <th>averageSales</th>
+            <?php
+                header('Content-Type: text/html; charset=UTF-8');
+                $mysqli=mysqli_connect("localhost","team21","team21","team21");
+                if(mysqli_connect_errno()){
+                    printf("Connect failed: %s\n", mysqli_error($mysqli));
+                    exit();
+                }
+                else{
+                    $selectedTypes = array();
+                    foreach($_POST["showValues"] as $type){
+                        if($type != "All"){
+                            array_push($selectedTypes, $type);
+                        }
+                    }
+                    $list = "";
+                    if(empty($selectedTypes)){
+                        $list = "Baking Goods', 'Breads', 'Breakfast', 'Canned', 'Dairy', 'Frozen Foods', 'Fruits and Vegetables', 'Hard Drinks', 
+                                'Health and Hygiene', 'Household', 'Meat', 'Seafood', 'Snack Foods', 'Soft Drinks', 'Starcky Foods', 'Others";
+                    }
+                    else{
+                        $list = implode("', '", $selectedTypes);
+                    }
+                    $sql = "SELECT iType, COUNT(iType) AS count, SUM(iOutletSales) AS totalSales, AVG(iOutletSales) AS averageSales
+                            FROM typeSales
+                            WHERE iType in ('".$list."')
+                            GROUP BY iType;";
+                    $res = mysqli_query($mysqli, $sql);
+                    if($res){
+                        while($newArray=mysqli_fetch_array($res, MYSQLI_ASSOC)){
+                            $iType=$newArray['iType'];
+                            $count=$newArray['count'];
+                            $totalSales = $newArray['totalSales'];
+                            $avgSales = $newArray['averageSales'];
+                            echo "<tr><td>".$iType."</td> <td>".$count."</td><td>".$totalSales."</td><td>".$avgSales."</td></tr>";
+                        }
+                    }
+                    else{
+                        printf("Could not retrieve records : %s\n", mysqli_error($mysqli));
+                    }
+                    mysqli_free_result($res);
+                    mysqli_close($mysqli);
+                }
+            ?>
+        </table>
+        <div class="radios">
+            <form action="type.php" method="POST">
+                <p><div class="show"> SHOW item types: </div></br>   
+                <label><input type= "checkbox" name = "showValues" value="All" > All</label></br>
+                <label><input type= "checkbox" name = "showValues" value="Baking Goods">Baking Goods</label></br>
+                <label><input type= "checkbox" name = "showValues" value="Breads">Breads</label></br>
+                <label><input type= "checkbox" name = "showValues" value="Breakfast">Breakfast</label></br>
+                <label><input type= "checkbox" name = "showValues" value="Canned"></label>Canned</br>
+                <label><input type= "checkbox" name = "showValues" value="Dairy"></label>Dairy</br>
+                <label><input type= "checkbox" name = "showValues" value="Frozen Foods"></label>Frozen Foods</br>
+                <label><input type= "checkbox" name = "showValues" value="Fruits and Vegetables"></label>Fruits and Vegetables</br>
+                <label><input type= "checkbox" name = "showValues" value="Hard Drinks"></label>Hard Drinks</br>
+                <label><input type= "checkbox" name = "showValues" value="Health and Hygiene"></label>Health and Hygiene</br>
+                <label><input type= "checkbox" name = "showValues" value="Household"></label>Household</br>
+                <label><input type= "checkbox" name = "showValues" value="Meat"></label>Meat</br>
+                <label><input type= "checkbox" name = "showValues" value="Seafood"></label>Seafood</br>
+                <label><input type= "checkbox" name = "showValues" value="Snack Foods"></label>Snack Foods</br>
+                <label><input type= "checkbox" name = "showValues" value="Soft Drinks"></label>Soft Drinks</br>
+                <label><input type= "checkbox" name = "showValues" value="Starcky Foods"></label>Starcky Foods</br>
+                <label><input type= "checkbox" name = "showValues" value="Others"></label>Others<p>
+                <input type="submit" name="submit" value="Run Analysis">
+        </div>
+    </body>
 </html>
