d2a50b149c66ebdd47823ad61ae9bea94c55210f
spig0126 <spig0126@gmail.com>
2022-11-14 23:47:47 +0900

    🚧 solving merge error

--- fat.php
+++ fat.php
@@ -6,76 +6,56 @@
     </head>
     <body>
         <div class="title">Sales information based on fat </div>
-            <table>
-                <th>fatContent</th>
-                <th>count</th>
-                <th>totalSales</th>
-                <th>averageSales</th>
+      
+<table>
+    <th>iFatContent</th>
+    <th>count(iFatContent)</th>
+    <th>totalSales</th>
+    <th>averageSales</th>
 
-            <?php
-                header('Content-Type: text/html; charset=UTF-8');
-                $mysqli=mysqli_connect("localhost","team21","team21","team21");
-                if(mysqli_connect_errno()){
-                    printf("Connect failed: %s\n", mysqli_error($mysqli));
-                    exit();
-                }
-                else{
-                    $showValues = "";
-                    if($_POST["showValues"] == "High Fat"){
-                        $showValues = "('High Fat')";
-                    }
-                    elseif($_POST["showValues"] == "Low Fat"){
-                        $showValues = "('Low Fat')";
-                    }
-                    elseif($_POST["showValues"] == "Regular"){
-                        $showValues = "('Regular')";
-                    }
-                    else{
-                        $showValues = "('Low Fat', 'Regular', 'High Fat')";
-                    }
+<?php
 
-                    $sql = "SELECT iFatContent, COUNT(iFatContent) AS cnt, SUM(iOutletSales) AS totalSales, AVG(iOutletSales) AS averageSales
-                    FROM fatSales
-                    WHERE iFatContent in ".$showValues."
-                    GROUP BY iFatContent;";
-                    $res = mysqli_query($mysqli, $sql);
-                    if($res){
+header('Content-Type: text/html; charset=UTF-8');
+$mysqli=mysqli_connect("localhost","team21","team21","team21");
+if(mysqli_connect_errno()){
+   printf("Connect failed: %s\n", mysqli_error($mysqli));
+   exit();
+}
+else{
+   
+   $sql = "SELECT iFatContent, COUNT(iFatContent) AS cnt, SUM(iOutletSales) AS totalSales, AVG(iOutletSales) AS averageSales
+   FROM fatSales
+   GROUP BY iFatContent;";
+   $res = mysqli_query($mysqli, $sql);
+   if($res){
+    while($newArray=mysqli_fetch_array($res, MYSQLI_ASSOC)){
+        $fatContent=$newArray['iFatContent'];
+        $count=$newArray['cnt'];
+        $sum=$newArray['totalSales'];
+        $average=$newArray['averageSales'];
+        echo "<tr><td>".$fatContent."</td> <td>".$count."</td> <td>".$sum."</td><td>".$average."</td></tr>";
+    }
+   }
+    else{
+        printf("Could not retrieve records : %s\n", mysqli_error($mysqli));
+    }
+    mysqli_free_result($res);
+    mysqli_close($mysqli);
+}
+?>
+</table>
 
-                        if(mysqli_num_rows($res) == 0){
-                            echo "There are no sales under the chosen conditions.";
-                        }
-                        else{
-                            while($newArray=mysqli_fetch_array($res, MYSQLI_ASSOC)){
-                                $fatContent=$newArray['iFatContent'];
-                                $count=$newArray['cnt'];
-                                $sum=$newArray['totalSales'];
-                                $average=$newArray['averageSales'];
-                                echo "<tr><td>".$fatContent."</td> <td>".$count."</td> <td>".$sum."</td><td>".$average."</td></tr>";
-                            }
+<div class="radios">
+    <form action="" method="POST">
+        <p><div class="show"> SHOW: </div></br>
+        
+        <label><input type= "radio" name = "showValues" vlaue="ALL" checked> ALL</label></br>
+        <label><input type= "radio" name = "showValues" vlaue="High Fat">High Fat<label></br>
+        <label><input type= "radio" name = "showValues" vlaue="Regular">Regular<label></br>
+        <label><input type= "radio" name = "showValues" vlaue="Low Fat">Low Fat<label><p>
+        <input type="submit" name="submit" value="Run Analysis">
+</div>
 
-                        }
-                        
-                    }
-                    else{
-                        printf("Could not retrieve records : %s\n", mysqli_error($mysqli));
-                    }
-                    mysqli_free_result($res);
-                    mysqli_close($mysqli);
-                }
-            ?>
-        </table>
-        </div>
-        <div class="radios">
-            <form action="" method="POST">
-                <p><div class="show"> SHOW: </div></br>
-                
-                <label><input type= "radio" name = "showValues" value="ALL" checked> ALL</label></br>
-                <label><input type= "radio" name = "showValues" value="High Fat">High Fat<label></br>
-                <label><input type= "radio" name = "showValues" value="Regular">Regular<label></br>
-                <label><input type= "radio" name = "showValues" value="Low Fat">Low Fat<label><p>
-                <input type="submit" name="submit" value="Run Analysis">
-        </div>
-
-    </body>
+</body>
 
 </html>
--- visibility.php
+++ visibility.php
@@ -7,7 +7,6 @@
 
 
     <body>
-        <?php $value =$_POST['range'];?>
         <div class="title">Sales information based on visibility </div>
       
 <table>
@@ -16,10 +15,10 @@
 </table>
 
 <div class="radios">
-    <form action="visibility.php" method="POST">
+    <form action="" method="POST">
         <p><div class="show"> Set sales range: </div></br>
         <div class="checkboxs">
-        <input type= range name = "range" min="1000" max="13000" value=<? if($value==null){echo "2000";}else{echo $value;}?> step="1000" id="myRange"> </br>
+        <input type= range name = "range" min="1000" max="13000" value="2000" step="1000" id="myRange"checked> </br>
         Value: <span id="value"></span></p>
         <input type="submit" name="submit" value="Run Analysis">
 </div>
