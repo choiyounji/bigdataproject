5cd9a48f7230ddb4edf008baa051a40502616e84
zziyuni <xhrl6671@gmail.com>
2022-11-17 02:04:54 +0900

    fix codes

--- insertItemSales.php
+++ insertItemSales.php
@@ -6,7 +6,7 @@
     </head>
     <body>
         <header>
-            <a href="select.html">
+            <a href="select.php">
                 <svg xmlns="http://www.w3.org/2000/svg" class="ionicon" width="30px" height="30px" ViewBox="0 0 512 512">
                     <path d="M80 212v236a16 16 0 0016 16h96V328a24 24 0 0124-24h80a24 24 0 0124 24v136h96a16 16 0 0016-16V212" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/>
                     <path d="M480 256L266.89 52c-5-5.28-16.69-5.34-21.78 0L32 256M400 179V64h-48v69" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/>
@@ -22,7 +22,6 @@
             </a>
         </header>
         <div class="title">Result of Insert Item Sales</div>
-
             <table>
                 <th>iIdentifier</th>
                 <th>oIdentifier</th>
@@ -44,9 +43,16 @@
                             echo "<tr><td></td><td> duplicated. </td></tr>";
                             exit(0);
                         }
-                        $sql = "INSERT into itemSales (iIdentifier,oIdentifier,iOutletSales,iVisibility,iMrp) values ('".$_POST["iIdentifier"]."','".$_POST["oIdentifier"]."','".$_POST["iOutletSales"]."','".$_POST["iVisibility"]."','".$_POST["iMrp"]."');";
-                        $res=mysqli_query($mysqli,$sql);
-                        if($res==TRUE){
+                        mysqli_begin_transaction($mysqli);
+                        try{
+                            mysqli_query($mysqli, "INSERT into itemInfo (iIdentifier,iWeight,iFatContent,iType) values ('".$_POST["iIdentifier"]."',".$_POST["iWeight"].",'".$_POST["iFatContent"]."','".$_POST["iType"]."');");
+                            $YearsEstablished = 2022-$_POST["oEstablishmentYear"];
+                            mysqli_query($mysqli, "INSERT into outlet (oIdentifier,oEstablishmentYear,oSize,oLocationType,oType,oYearsEstablished) values ('".$_POST["oIdentifier"]."','".$_POST["oEstablishmentYear"]."','".$_POST["oSize"]."','".$_POST["oLocationType"]."','".$_POST["oType"]."','".$YearsEstablished."');");
+                            mysqli_query($mysqli, "INSERT into itemSales (iIdentifier,oIdentifier,iOutletSales,iVisibility,iMrp) values ('".$_POST["iIdentifier"]."','".$_POST["oIdentifier"]."','".$_POST["iOutletSales"]."',".$_POST["iVisibility"].",".$_POST["iMrp"].");");
+                            mysqli_commit($mysqli);
+                            printf("commit done");
+            
+        
                             $sql = "SELECT * FROM itemSales WHERE iIdentifier='".$_POST['iIdentifier']."' AND oIdentifier='".$_POST['oIdentifier']."';";
                             $res = mysqli_query($mysqli, $sql);
                             if($res){
@@ -64,14 +70,17 @@
                                     printf("Could not retrieve records : %s\n", mysqli_error($mysqli));
                                 }
                             
-                        }
-                        else{
-                            printf("Could not insert record : %s\n", mysqli_error($mysqli));
-                        }
-                        mysqli_free_result($ret);
+                            }  catch(mysqli_sql_exception $exception){
+                                mysqli_rollback($mysqli);
+                                echo "<tr><td></td><td> Syntax ERROR </td></tr>";
+                             
+                            }
+        
+                  
                         mysqli_free_result($res);
                         mysqli_close($mysqli);
                     }
+
                 ?>
             </table>
     </body>
